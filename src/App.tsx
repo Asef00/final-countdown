a8d3f89da4d0c59331f07e54ec943a462902a4da
Asef00 <asefhosseini00@gmail.com>
2024-11-14 12:48:51 +0330

    remove boilerplate code, first usage of material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
 import './App.css'
+import { Button } from '@mui/material'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-
+      <Button variant='contained'>Hello World</Button>
     </>
   )
 }
