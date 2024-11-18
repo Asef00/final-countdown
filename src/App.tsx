17b8e9cc34c9ac888ff3337f86f4abb9419093ee
Asef00 <asefhosseini00@gmail.com>
2024-11-18 17:08:49 +0330

    Implement initial ui using Mui components, define and use a defualt theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,59 @@
+import { useState } from 'react'
 import './App.css'
-import { Card, CardContent, Typography } from '@mui/material'
+import {
+  Button,
+  Card,
+  CardContent,
+  Container,
+  createTheme,
+  Stack,
+  TextField,
+  ThemeProvider,
+  Typography,
+} from '@mui/material'
+
+const defaultTheme = createTheme({
+  typography: {
+    h1: {
+      fontSize: '4rem',
+    },
+  },
+})
 
 function App() {
+  const [name, setName] = useState('')
+
   return (
     <>
-      <Card>
-        <CardContent>
-          <Typography textTransform={'uppercase'} variant="h1">
-            The Almost final countdown
-          </Typography>
-          <Typography variant="body1">
-            Stop the timer once you estimate that time is (almost) up
-          </Typography>
-        </CardContent>
-      </Card>
+      <ThemeProvider theme={defaultTheme}>
+        <Container>
+          <Card sx={{ maxWidth: 1000, textAlign: 'center' }}>
+            <CardContent>
+              <Typography
+                textTransform={'uppercase'}
+                variant="h1"
+                fontSize="3rem"
+                gutterBottom
+              >
+                The Almost final countdown
+              </Typography>
+              <Typography variant="body1">
+                Stop the timer once you estimate that time is (almost) up
+              </Typography>
+              <Typography marginBottom={3}>Welcome {name}!</Typography>
+              <Stack direction="row" justifyContent="center" spacing={1}>
+                <TextField
+                  size="small"
+                  id="name"
+                  label="Name"
+                  variant="outlined"
+                />
+                <Button variant="contained">Set Name</Button>
+              </Stack>
+            </CardContent>
+          </Card>
+        </Container>
+      </ThemeProvider>
     </>
   )
 }
